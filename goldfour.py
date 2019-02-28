4626cb58cc565e51627627bda3191e833f78a763
madziakk77 <42512679+madziakk77@users.noreply.github.com>
2019-02-28 18:54:29 +0100

    Create goldfour.py

---
+++ goldfour.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+rowA=[1, 2, 3, 4, 5, 6]
+rowB=[10, 11, 12, 13, 14, 15, 16]
+rowC=[20, 21, 22, 23, 24, 25, 26]
+rowD=[30, 31, 32, 33, 34, 35, 36]
+
+for i in A B C D:
+//teoreticly:  string("row"+$i)[i]+the same for each string in for loop as well
