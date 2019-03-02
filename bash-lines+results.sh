e02a379218b93e23e510f2f73e87ec2c4bef0320
madziakk77 <42512679+madziakk77@users.noreply.github.com>
2019-03-02 08:47:38 +0100

    Update bash-lines+results.sh

--- bash-lines+results.sh
+++ bash-lines+results.sh
@@ -4,7 +4,21 @@ for number in {1 2}; do cp file.txt file-$number.txt; done
 //result:
 //file-{1.txt
 //file-2}.txt
+
 for number in {1..2}; do cp file.txt file-$number.txt; done
 //result:
 //file-1.txt
 //file-2.txt
+
+for i in $('ls'); do echo plik czy katalog: $i; done
+//results:
+//plik czy katalog: Desktop
+//plik czy katalog: Documents
+//plik czy katalog: Downloads
+//plik czy katalog: Music
+//plik czy katalog: Pictures
+//plik czy katalog: Public
+//plik czy katalog: Templates
+//plik czy katalog: Videos
+//plik czy katalog: VirtualBox
+//plik czy katalog: wstep.odt
